b76a632c363a49a76fcc08cccec4cfa552a643b6
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-02-29 17:48:28 +0100

    delay load pdf (fix2)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-window.onload() = function() {
-	setAge();
+window.onload = function() {
+/*	setAge();*/
 	addPrintTrigger();
-}
+};
 
 function printTrigger(elementId) {
     var getMyFrame = document.getElementById(elementId);
     getMyFrame.focus();
     getMyFrame.contentWindow.print();
-}
+};
 
 function addPrintTrigger() {
-	var getMain = document.getElementByTagName('main')[0];
+	var getMain = document.getElementsByTagName('main')[0];
+	console.log(getMain);
 	var iFramePdf = document.createElement('iframe');
-    iFramePdf.id = "dfghjkl";
-    getMain.appendChild('iFramePdf');
-}
+    iFramePdf.id = "iFramePdf";
+    iFramePdf.src = "Nicolas-Marshall-Resume.pdf";
+    iFramePdf.style.display = 'none';
+    console.log(iFramePdf);
+    document.getElementsByTagName('body')[0].appendChild(iFramePdf);
+};
