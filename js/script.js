abbfa21f6ea6b04e9fa28a1787c723acfdb577cd
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-02-29 14:07:15 +0100

    delayed pdf load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+window.onload() = function() {
+	setAge();
+	addPrintTrigger();
+}
+
 function printTrigger(elementId) {
     var getMyFrame = document.getElementById(elementId);
     getMyFrame.focus();
     getMyFrame.contentWindow.print();
+}
+
+function addPrintTrigger() {
+	var getMain = document.getElementByTagName('main')[0];
+	var iFramePdf = document.createElement('iframe');
+    iFramePdf.id = "dfghjkl";
+    getMain.appendChild('iFramePdf');
 }
