65eb67702364b66cb03107f4d6e470baee1f3783
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-03-05 09:19:33 +0100

    modal : keydown and other js transitions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
 window.onload = function() {
     setAge();
-    addPrintTrigger();
+    addPrintIframe();
     addEventListeners();
 };
 
-function printTrigger(elementId) {
+var contactLayer = document.getElementById('contact-layer');
+var contact = document.getElementById('contact');
+var emailField = document.getElementById('email-field');
+var messageField = document.getElementById('message-field');
+var submitButton = document.getElementById('submit');
+var isContactActive = false;
+
+function addEventListeners() {
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener('click', clickHandler, false);
+};
+
+function keyDownHandler(e) {
+    var keyCode = e.which;
+    if (keyCode == 27) {
+        closeContact();
+        console.log(e.target)
+    }
+    if (keyCode == 13 && e.target === emailField) {
+        e.preventDefault();
+        if (emailField.checkValidity()) {
+            messageField.focus();
+        } else {
+        	submitButton.click();
+        }
+    }
+};
+
+function clickHandler(e) {
+    if (isContactActive && e.target === contactLayer) {
+        closeContact();
+    }
+};
+
+function triggerPrint(elementId) {
     var getMyFrame = document.getElementById(elementId);
     getMyFrame.focus();
     getMyFrame.contentWindow.print();
 };
 
-function addPrintTrigger() {
+function addPrintIframe() {
     var iFramePdf = document.createElement('iframe');
     iFramePdf.id = "iFramePdf";
     iFramePdf.src = "pdf/Nicolas-Marshall-Resume.pdf";
     iFramePdf.style.display = 'none';
-    document.getElementsByTagName('body')[0].appendChild(iFramePdf);
+    document.body.appendChild(iFramePdf);
 };
 
 function setAge() {
@@ -29,23 +63,15 @@ function setAge() {
     document.getElementById('age').textContent = age;
 };
 
-var isContactActive = false;
-
 function toggleContact() {
-	isContactActive = !isContactActive;
-	document.getElementsByClassName('contact-layer')[0].classList.toggle('dim');
-	document.getElementsByClassName('contact')[0].classList.toggle('active');
-	document.getElementById("form-email").focus();
-};
-
-function addEventListeners() {
-	document.addEventListener()
+    isContactActive = !isContactActive;
+    contactLayer.classList.toggle('dim');
+    contact.classList.toggle('active');
+    emailField.focus();
 };
 
 function closeContact() {
-	if (isContactActive) {
-		toggleContact();
-	}
+    if (isContactActive) {
+        toggleContact();
+    }
 };
-
-
