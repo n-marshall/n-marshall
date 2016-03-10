a068b295927f9cf53d4e713765df2c1710488969
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-03-10 16:03:40 +0100

    form fixes, test pending

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ var emailField = document.getElementById('email-field');
 var messageField = document.getElementById('message-field');
 var submitButton = document.getElementById('submit');
 
+var message = new Object();
+message.loading = 'Loading...';
+message.success = 'Thank you. Application received!';
+message.failure = 'Whoops! There was a problem sending your message.';
+var statusMessage = document.createElement('div');
+statusMessage.className = 'status';
+
+// Set up the AJAX request
+var request = new XMLHttpRequest();
+request.open('POST', 'http://formspree.io/wot@mailinator.com', true);
+request.setRequestHeader('accept', 'application/json');
+
 window.onload = function() {
     setAge();
     contactForm.isActive = false;
@@ -17,6 +29,52 @@ function addEventListeners() {
     document.addEventListener('focusout', focusOutHandler);
     document.addEventListener('backbutton', backButtonHandler);
     contactForm.addEventListener('submit', submitHandler);
+}
+
+/*function postForm(form) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://formspree.io/wot@mailinator.com');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control')
+    var xhrBody = { '_replyto': contactForm._replyto.value, 'message': contactForm.message.value };
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            alert(xhr.responseText);
+        } else {
+            console.log(Error(xhr.statusText));
+        }
+    }
+    xhr.onerror = function() {
+        console.error(Error("Network error"));
+    }
+    xhr.send(xhrBody);
+}*/
+
+function submitHandler(e) {
+    e.preventDefault();
+    contactForm.appendChild(statusMessage);
+
+    // Create a new FormData object passing in the form's key value pairs (that was easy!)
+    var formData = new FormData(contactForm);
+
+    // Send the formData
+    request.send(formData);
+
+    // Watch for changes to request.readyState and update the statusMessage accordingly
+    request.onreadystatechange = function() {
+        // <4 =  waiting on response from server
+        if (request.readyState < 4)
+            statusMessage.innerHTML = message.loading;
+        // 4 = Response from server has been completely loaded.
+        else if (request.readyState === 4) {
+            // 200 - 299 = successful
+            if (request.status == 200 && request.status < 300)
+                statusMessage.innerHTML = message.success;
+            else
+                contactForm.insertAdjacentHTML('beforeend', message.failure);
+        }
+    }
 }
 
 function keyDownHandler(e) {
@@ -37,10 +95,7 @@ function keyDownHandler(e) {
 function clickHandler(e) {
     if (contactForm.isActive && e.target === contactLayer) {
         closeContact();
-    }/*
-    if (e.target === messageField) {
-        console.log(contactForm.message)
-    }*/
+    }
 }
 
 function focusOutHandler(e) {
@@ -57,11 +112,6 @@ function updateValidityHint(field) {
             field.parentNode.classList.add('invalid');
         }
     }
-}
-
-function submitHandler(e) {
-    e.preventDefault();
-    postForm(contactForm);
 }
 
 function backButtonHandler() {
