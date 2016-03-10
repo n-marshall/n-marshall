f2d4584cb70a8c6d5ce6d977518c5da57c513d70
Nicolas Marshall <marshall.nicolas@gmail.com>
2016-03-10 16:36:39 +0100

    form now working, added success message

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ var submitButton = document.getElementById('submit');
 
 var message = new Object();
 message.loading = 'Loading...';
-message.success = 'Thank you. Application received!';
+message.success = 'Thank you. I\'ll get back to you as soon as possible';
 message.failure = 'Whoops! There was a problem sending your message.';
 var statusMessage = document.createElement('div');
 statusMessage.className = 'status';
 
 // Set up the AJAX request
 var request = new XMLHttpRequest();
-request.open('POST', 'http://formspree.io/wot@mailinator.com', true);
+request.open('POST', 'http://formspree.io/marshall.nicolas@gmail.com', true);
 request.setRequestHeader('accept', 'application/json');
 
 window.onload = function() {
@@ -31,44 +31,15 @@ function addEventListeners() {
     contactForm.addEventListener('submit', submitHandler);
 }
 
-/*function postForm(form) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://formspree.io/wot@mailinator.com');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Access-Control')
-    var xhrBody = { '_replyto': contactForm._replyto.value, 'message': contactForm.message.value };
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            alert(xhr.responseText);
-        } else {
-            console.log(Error(xhr.statusText));
-        }
-    }
-    xhr.onerror = function() {
-        console.error(Error("Network error"));
-    }
-    xhr.send(xhrBody);
-}*/
-
 function submitHandler(e) {
     e.preventDefault();
     contactForm.appendChild(statusMessage);
-
-    // Create a new FormData object passing in the form's key value pairs (that was easy!)
     var formData = new FormData(contactForm);
-
-    // Send the formData
     request.send(formData);
-
-    // Watch for changes to request.readyState and update the statusMessage accordingly
     request.onreadystatechange = function() {
-        // <4 =  waiting on response from server
         if (request.readyState < 4)
             statusMessage.innerHTML = message.loading;
-        // 4 = Response from server has been completely loaded.
         else if (request.readyState === 4) {
-            // 200 - 299 = successful
             if (request.status == 200 && request.status < 300)
                 statusMessage.innerHTML = message.success;
             else
